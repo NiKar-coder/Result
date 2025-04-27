ea844ba78a9add23bfe42814c0a6afe751281d9a
NiKar-coder <Nikitakarataev22@gmail.com>
2025-04-27 13:33:56 +0300

    Update

---
+++ solution.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def promotion():
+    arr = [
+        "Человечество вырастает из детства.",
+        "Человечеству мала одна планета.",
+        "Мы сделаем обитаемыми безжизненные пока планеты.",
+        "И начнем с Марса!",
+        "Присоединяйся!"]
+    return '</br></br>'.join(arr)
+
+
+@app.route('/image_mars')
+def image_mars():
+    return render_template('mars.html')
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    return render_template('promotion_image.html')
+
+
+@app.route('/choice/<variant>')
+def choice(variant):
+    return render_template('choice.html', variant=variant)
+
+
+@app.route("/astronaut_selection", methods=("POST", "GET"))
+def astronaut_selection():
+    if request.method == "GET":
+        return render_template("astronaut_selection.html")
+    elif request.method == "POST":
+        print(request.form)
+        return "OK"
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
